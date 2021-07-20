11bf9f58b876bb134145e4c24484d4f2c529ec17
AlexKhlybov <aghlybov@gmail.com>
2021-07-20 13:09:43 +0300

    Added template and static files

--- electroplace/settings.py
+++ electroplace/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '76hm%gs)y43%+6ds7tkjq_=9$sa1jcn88nsqpv%)vs%azpkxs2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -122,4 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
