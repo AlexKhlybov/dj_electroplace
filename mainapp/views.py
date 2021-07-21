4f2cad3a76467ef60376ff0f530b686f679ffb9b
AlexKhlybov <aghlybov@gmail.com>
2021-07-22 00:46:33 +0300

    Added data from db

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 
-from .models import Brands, PromoSlider
+from .models import Benefits, Brands, Contacts, News, Products, PromoSlider
 
-
+  
 class HomePage(ListView):
     model = PromoSlider
     template_name = 'mainapp/index.html'
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['title'] = 'electroplace'
         context['promo'] = PromoSlider.objects.all()
         context['brands'] = Brands.objects.all().exclude(image__exact='')
+        context['top_news'] = News.objects.all().first()
+        context['news'] = News.objects.all().exclude(id=context['top_news'].id)
+        context['hits'] = Products.get_stock_products()
+        context['benefits'] = Benefits.objects.all()[:3]
+        context['contact'] = Contacts.objects.first()
         return context
 
 
