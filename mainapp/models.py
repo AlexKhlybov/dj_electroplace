3d445df9c3c88adcbeb377f82ba337f6284c136b
AlexKhlybov <aghlybov@gmail.com>
2021-07-27 17:16:51 +0300

    Fix bugs

--- mainapp/models.py
+++ mainapp/models.py
@@ -4,16 +4,19 @@ from random import sample
 from django.db import models
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
-from django.utils.translation import gettext_lazy as _
 
 
 class ProductsCategory(models.Model):
-    name = models.CharField(verbose_name="name_category", max_length=32, unique=True)
-    desc_category = models.TextField(verbose_name="description", blank=True)
-    is_active = models.BooleanField(verbose_name="category_is_active", db_index=True, default=True)
+    name = models.CharField(verbose_name="Категория", max_length=32, unique=True)
+    desc_category = models.TextField(verbose_name="Описание", blank=True)
+    is_active = models.BooleanField(verbose_name="Активная", db_index=True, default=True)
 
-    created_at = models.DateTimeField(verbose_name="Created", auto_now_add=True)
-    updated_at = models.DateTimeField(verbose_name="Updated", auto_now=True)
+    created_at = models.DateTimeField(verbose_name="Создан", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="Обновлен", auto_now=True)
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
 
     def __str__(self):
         return self.name
@@ -24,16 +27,17 @@ class ProductsCategory(models.Model):
 
 
 class Brands(models.Model):
-    name = models.CharField(verbose_name=_("name"), max_length=32, unique=True)
-    desc_brands = models.TextField(verbose_name=_("description"), blank=True)
-    image = models.ImageField(upload_to=_("images"), blank=True)
-    is_active = models.BooleanField(verbose_name="brands_is_active", db_index=True, default=True)
+    name = models.CharField(verbose_name="Название", max_length=32, unique=True)
+    desc_brands = models.TextField(verbose_name="Описание", blank=True)
+    image = models.ImageField(upload_to="Изображение", blank=True)
+    is_active = models.BooleanField(verbose_name="Активный", db_index=True, default=True)
 
-    created_at = models.DateTimeField(verbose_name="Created", auto_now_add=True)
-    updated_at = models.DateTimeField(verbose_name="Updated", auto_now=True)
+    created_at = models.DateTimeField(verbose_name="Создан", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="Обновлен", auto_now=True)
 
     class Meta:
-        verbose_name_plural = _("Brands")
+        verbose_name = "Бренд"
+        verbose_name_plural = "Бранды"
 
     def __str__(self):
         return self.name
@@ -51,25 +55,26 @@ class Products(models.Model):
     category = models.ForeignKey(ProductsCategory, on_delete=models.CASCADE)
     brands = models.ForeignKey(Brands, on_delete=models.CASCADE)
 
-    name = models.CharField(verbose_name="product", max_length=128)
-    image = models.ImageField(upload_to="product_images", blank=True)
-    price = models.DecimalField(verbose_name="price", max_digits=8, decimal_places=2, default=0)
-    old_price = models.DecimalField(verbose_name="old_price", max_digits=8, decimal_places=2, default=0, blank=True)
-    screen = models.DecimalField(verbose_name="screen", max_digits=3, decimal_places=2, default=0, blank=True)
-    sim = models.PositiveIntegerField(verbose_name="sim_qty", default=0, blank=True)
-    storage = models.PositiveIntegerField(verbose_name="storage", default=0, blank=True)
-    color = models.CharField(verbose_name="color", max_length=128, blank=True)
-    quantity = models.PositiveIntegerField(verbose_name="quantity", default=0)
+    name = models.CharField(verbose_name="Продукт", max_length=128)
+    image = models.ImageField(upload_to="Изображение", blank=True)
+    price = models.DecimalField(verbose_name="Цена", max_digits=8, decimal_places=2, default=0)
+    old_price = models.DecimalField(verbose_name="Старая цена", max_digits=8, decimal_places=2, default=0, blank=True)
+    screen = models.DecimalField(verbose_name="Экран", max_digits=3, decimal_places=2, default=0, blank=True)
+    sim = models.PositiveIntegerField(verbose_name="Кол-во сим карт", default=0, blank=True)
+    storage = models.PositiveIntegerField(verbose_name="Хранилище", default=0, blank=True)
+    color = models.CharField(verbose_name="Цвет", max_length=128, blank=True)
+    quantity = models.PositiveIntegerField(verbose_name="Кол-во", default=0)
 
-    is_stock = models.BooleanField(verbose_name="product_sale", db_index=True, default=False)
-    is_active = models.BooleanField(verbose_name="product_activ", db_index=True, default=True)
-    is_new = models.BooleanField(verbose_name="product_new", db_index=True, default=False)
+    is_stock = models.BooleanField(verbose_name="В акции", db_index=True, default=False)
+    is_new = models.BooleanField(verbose_name="Новинка", db_index=True, default=False)
+    is_active = models.BooleanField(verbose_name="Активный", db_index=True, default=True)
 
-    created_at = models.DateTimeField(verbose_name="Created", auto_now_add=True)
-    updated_at = models.DateTimeField(verbose_name="Updated", auto_now=True)
+    created_at = models.DateTimeField(verbose_name="Создан", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="Обновлен", auto_now=True)
 
     class Meta:
-        verbose_name_plural = _("Products")
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
 
     def __str__(self):
         return f"{self.name} {self.storage} {self.color}"
@@ -90,17 +95,18 @@ class Products(models.Model):
 
 
 class News(models.Model):
-    title = models.CharField(verbose_name="title_news", max_length=128)
-    subtitle = models.CharField(verbose_name="subtitle_news", max_length=128)
-    text = models.TextField(verbose_name="description", blank=True)
-    image = models.ImageField(upload_to="news_images", blank=True)
-    is_active = models.BooleanField(verbose_name="product_activ", db_index=True, default=True)
+    title = models.CharField(verbose_name="Заголовок", max_length=128)
+    subtitle = models.CharField(verbose_name="Подзаголовок", max_length=128)
+    text = models.TextField(verbose_name="Текст", blank=True)
+    image = models.ImageField(upload_to="Изображение", blank=True)
+    is_active = models.BooleanField(verbose_name="Активная", db_index=True, default=True)
 
-    public_date = models.DateTimeField(False, default=timezone.now)
-    updated_at = models.DateTimeField(verbose_name="Updated", auto_now=True)
+    public_date = models.DateTimeField(verbose_name="Дата публикации",  default=timezone.now)
+    updated_at = models.DateTimeField(verbose_name="Обновлено", auto_now=True)
 
     class Meta:
-        verbose_name_plural = _("News")
+        verbose_name = "Новость"
+        verbose_name_plural = "Новости"
 
     def __str__(self):
         return f"{self.title}"
@@ -111,48 +117,45 @@ class News(models.Model):
 
 
 class Benefits(models.Model):
-    title = models.CharField(verbose_name="title", max_length=128)
-    desc = models.TextField(verbose_name="description", blank=True)
+    title = models.CharField(verbose_name="Заголовок", max_length=128)
+    desc = models.TextField(verbose_name="Текст", blank=True)
 
     class Meta:
-        verbose_name = _("Advantage")
-        verbose_name_plural = _("Benefits")
+        verbose_name = "Преимущество"
+        verbose_name_plural = "Преимущества"
 
     def __str__(self):
         return f"{self.title}"
 
 
 class Contacts(models.Model):
-    city = models.CharField(verbose_name="city", max_length=256)
-    phone = models.CharField(verbose_name="phone", max_length=20)
+    city = models.CharField(verbose_name="Город", max_length=256)
+    phone = models.CharField(verbose_name="Телефон", max_length=20)
     email = models.CharField(verbose_name="e-mail", max_length=128)
-    address = models.CharField(verbose_name="address", max_length=512)
-    timetable = models.CharField(verbose_name="timetable", max_length=256)
+    address = models.CharField(verbose_name="Адрес", max_length=512)
+    timetable = models.CharField(verbose_name="Часы работы", max_length=256)
+
+    class Meta:
+        verbose_name = "Контакт"
+        verbose_name_plural = "Контакты"
 
     def __str__(self):
         return f"{self.city}, {self.address}"
 
-    class Meta:
-        verbose_name = _("Contact")
-        verbose_name_plural = _("Contacts")
-
-    def __str__(self):
-        return f"{self.city}"
-
 
 class PromoSlider(models.Model):
-    name = models.CharField(verbose_name=_("name"), max_length=256)
+    name = models.CharField(verbose_name="Название", max_length=256)
     brand = models.ForeignKey(Brands, on_delete=models.CASCADE)
-    sort_desc = models.CharField(verbose_name=_("sort_desc"), max_length=256)
-    image = models.ImageField(upload_to="promo_images", blank=True)
-    is_active = models.BooleanField(verbose_name="product_activ", db_index=True, default=True)
+    sort_desc = models.CharField(verbose_name="Короткое описание", max_length=256)
+    image = models.ImageField(upload_to="Изображение", blank=True)
+    is_active = models.BooleanField(verbose_name="Активный", db_index=True, default=True)
 
-    public_date = models.DateTimeField(False, default=timezone.now)
-    updated_at = models.DateTimeField(verbose_name="Updated", auto_now=True)
+    public_date = models.DateTimeField(verbose_name="Дата публикации", default=timezone.now)
+    updated_at = models.DateTimeField(verbose_name="Обновлено", auto_now=True)
 
     class Meta:
-        verbose_name = _("Promo")
-        verbose_name_plural = _("Promotions")
+        verbose_name = "Акция слайд"
+        verbose_name_plural = "Акции слайд"
 
     def __str__(self):
         return f"{self.name} ({self.public_date})"
